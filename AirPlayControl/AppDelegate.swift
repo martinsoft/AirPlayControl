60db9533f8bd22368a81101f4ef78d1a0b8e8be9
John Martin <john@martinsoft.net>
2017-01-28 16:16:06 +0000

    Initial Commit

---
+++ AirPlayControl/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AirPlayControl
+//
+//  Created by John Martin on 28/01/2017.
+//  Copyright © 2017 Martinsoft Limited. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
