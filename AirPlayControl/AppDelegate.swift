098aebb25080cd36b0ecb148585b08882e97e391
John Martin <john@martinsoft.net>
2017-01-28 16:47:32 +0000

    Initial Commit. Base application now works, builds apple script and can set the airplay display. Feels like overkill, needs simplification.

--- AirPlayControl/AppDelegate.swift
+++ AirPlayControl/AppDelegate.swift
@@ -13,15 +13,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        let options : NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true]
+        let accessibilityEnabled = AXIsProcessTrustedWithOptions(options)
+        
+        if (!accessibilityEnabled) {
+            NSApplication.shared().terminate(self)
+        }
+        
+        let deviceName = CommandLine.arguments[1]
+        var source = ""
+        
+        source += "use AppleScript version \"2.4\" -- Yosemite (10.10) or later" + "\n"
+        source += "use scripting additions" + "\n"
+        source += "set device_name to \"" + deviceName + "\"" + "\n"
+        source += "say device_name" + "\n"
+        source += "tell application \"System Events\"" + "\n"
+        source += "  tell process \"SystemUIServer\"" + "\n"
+        source += "    set display_menu to menu bar item 1 of menu bar 1 whose description contains \"Displays Menu\"" + "\n"
+        source += "    click display_menu" + "\n"
+        source += "    set the_menu to menu 1 of result" + "\n"
+        source += "    delay 1.0" + "\n"
+        source += "    if menu item device_name of the_menu exists then" + "\n"
+        source += "      click menu item device_name of the_menu" + "\n"
+        source += "    else" + "\n"
+        source += "      key code 53" + "\n"            // Close the AirPlay menu again if not found
+        source += "    end if" + "\n"
+        source += "  end tell" + "\n"
+        source += "end tell" + "\n"
+        
+        let script = NSAppleScript(source: source)
+        var error: NSDictionary?
+        let result = script?.executeAndReturnError(&error)
+        
+        if result == nil {
+            print("Failed to change AirPlay device")
+            if (error != nil) {
+                print("\(error)")
+            }
+        }
+        NSApplication.shared().terminate(self)
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
 
